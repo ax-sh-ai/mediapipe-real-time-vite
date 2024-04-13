ca97076f494f90feb5ef576ac098be8ab8b7366b
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-13 22:06:31 +0545

    feat: Add vitest to package.json and configure vitest in vite.config.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-
 import { useFaceDetector } from './hooks/use-face-detector';
 
 function App() {
   const faceDetector = useFaceDetector();
 
-  return <><div className='bg-blue-400'>ss</div></>;
+  return (
+    <>
+      <div className='bg-blue-400'>ss</div>
+    </>
+  );
 }
 
 export default App;
