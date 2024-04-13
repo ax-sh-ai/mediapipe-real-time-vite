2ce64dbdd15d86f4d19ef479007ea310dc5b7790
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-13 23:51:35 +0545

    test: Add test to upload multiple files in App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,31 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import App from './App';
 
 // Tests
-describe.todo('Renders main page correctly', async () => {
-  it('Should render the page correctly', async () => {
+describe('Renders main page correctly', async () => {
+  it.todo('Should render the page correctly', async () => {
     // Setup
     render(<App />);
     const h1 = screen.queryByText('Vite + React');
 
     // Expectations
     expect(h1).not.toBeNull();
+  });
+  it('upload test files', () => {
+    const files = [
+      new File(['hello'], 'hello.geojson', { type: 'application/json' }),
+      new File(['there'], 'hello2.geojson', { type: 'application/json' })
+    ];
+
+    render(<App />);
+    const input = screen.getByTestId<HTMLInputElement>('dropzone');
+    userEvent.upload(input, files);
+    console.log(input.files);
+    console.log(input);
+    // expect(input.files).toHaveLength(2);
+    // expect(input.files[0]).toStrictEqual(files[0]);
+    // expect(input.files[1]).toStrictEqual(files[1]);
   });
 });
