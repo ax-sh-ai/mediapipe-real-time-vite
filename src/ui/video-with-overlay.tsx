8fc6927cf8a4c12cccb0fb5119357a81ecc9a5be
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-19 14:30:31 +0545

    refactor: Update callback function parameter to include video element and detections in an object

--- src/ui/video-with-overlay.tsx
+++ src/ui/video-with-overlay.tsx
@@ -1,7 +1,6 @@
-import { Detection } from '@mediapipe/tasks-vision';
 import { ComponentProps, useCallback, useState } from 'react';
 
-import { useVideoFrameHook } from '../hooks/use-video-frame-hook.ts';
+import { DetectionCallbackArgs, useVideoFrameHook } from '../hooks/use-video-frame-hook.ts';
 import { boundingBoxToPolygonPoints } from '../libs/utils.tsx';
 import { ExtendDetection } from '../types.ts';
 import { SvgOverlay } from './svg-overlay.tsx';
@@ -43,7 +42,7 @@ function DetectionBox(detection: ExtendDetection) {
 export function VideoWithOverlay(props: VideoProps) {
   const [detections, setDetections] = useState<ExtendDetection[]>([]);
   const [state, setState] = useState({ height: 0, width: 0 });
-  const callback = useCallback((video: HTMLVideoElement, detections: Detection[]) => {
+  const callback = useCallback(({ video, detections }: DetectionCallbackArgs) => {
     const { clientWidth, clientHeight, videoWidth, videoHeight, offsetWidth, offsetHeight } = video;
     setState({ width: videoWidth, height: videoHeight });
     setDetections(
