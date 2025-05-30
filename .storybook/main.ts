ba9c2914b22b7f95776b6452f5c02781a5b8ad2a
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-05-30 16:06:08 +0545

    chore: update Storybook configuration and dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,20 +2,24 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
-    '@storybook/addon-onboarding',
-    '@storybook/addon-links',
+    // '@storybook/addon-onboarding',
+    // '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
-    '@storybook/addon-interactions'
+    // '@chromatic-com/storybook',
+    // '@storybook/addon-interactions'
   ],
+
   framework: {
     name: '@storybook/react-vite',
     options: {}
   },
-  docs: {
-    autodocs: 'tag'
+  core:{
+    disableTelemetry: true, // 👈 Disables telemetry
+    enableCrashReports: false, // 👈 Appends the crash reports to the telemetry events
   },
+
   staticDirs: ['../public']
 };
 export default config;
